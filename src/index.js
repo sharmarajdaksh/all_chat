3d86147ea612a24c3db7c7a2379db0ad81ecc4ac
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2019-03-23 13:02:18 +0530

    Router added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css'; // sets overall global styles such as font 
 import App from './App';
@@ -19,7 +20,7 @@ const rootReducer = combineReducers({
 // create a store using the combined reducers
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><Provider store={store}><App /></Provider></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
