bf1454f3b5586fd275d58c681f322e050e90920b
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2019-03-23 13:12:21 +0530

    Attempt to get pusblishing working

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
 
 import './index.css'; // sets overall global styles such as font 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import chatReducer from './store/reducers/chat';
 import nameReducer from './store/reducers/name';
+
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 // assign tags to individual reducers and combine
 const rootReducer = combineReducers({
@@ -20,7 +23,7 @@ const rootReducer = combineReducers({
 // create a store using the combined reducers
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><Provider store={store}><App /></Provider></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL} history={hashHistory}><Provider store={store}><App /></Provider></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
